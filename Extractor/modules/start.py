76088b4ef3dcec76dc67222ebfbc3adf728519c0
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-02-20 17:00:09 +0530

    Update start.py

--- Extractor/modules/start.py
+++ Extractor/modules/start.py
@@ -11,9 +11,32 @@ buttons = InlineKeyboardMarkup([
                  InlineKeyboardButton("🔗 Support", url="https://t.me/DevsHubChat")
               ])
 
+button = InlineKeyboardMarkup([
+              [
+                 InlineKeyboardButton("🔖 Home", callback_data="home_")
+              ])
+
+
 
 @app.on_message(filters.command("start"))
 async def start_(_, message):
+  name = message.from_user.mention
   await message.reply_photo(photo=script.PHOTO, 
-         caption=script.START_TEXT, reply_markup=buttons)
+         caption=script.START_TEXT.format(name), reply_markup=buttons)
 
+
+
+
+@app.on_callback_query()
+async def handle_callback(_, query):
+  name = query.from_user.mention
+  if query.data=="home_":
+    await query.message.edit_text(
+          script.START_TEXT.format(name),
+          reply_markup=button)
+
+  elif query.data=="help_":
+    await query.message.edit_text(
+          script.HELP_TEXT,
+          reply_markup=button)
+        
