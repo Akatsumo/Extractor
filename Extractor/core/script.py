6722d462ef7adfb35e5bf23f68ce7d52a979ceb2
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-02-20 17:10:20 +0530

    Create script.py

---
+++ Extractor/core/script.py
@@ -0,0 +1,28 @@
+
+PHOTO = "https://envs.sh/djQ.jpg"
+
+# ------------------------------------------------------------ #
+
+START_TXT = """
+**ʜᴇʟʟᴏ** {},
+
+**ɪ'ᴍ ᴀᴅᴇᴘᴛ ᴀᴛ ᴇxᴛʀᴀᴄᴛɪɴɢ ᴏɴʟɪɴᴇ ᴄᴏᴜʀsᴇs ᴡɪᴛʜ ᴄᴜᴛᴛɪɴɢ-ᴇᴅɢᴇ ᴛᴇᴄʜɴᴏʟᴏɢʏ. ɪ'ᴠᴇ ᴍᴀɴᴀɢᴇᴅ ᴛᴏ ɢᴀᴛʜᴇʀ ʟɪɴᴋs ғᴏʀ ᴀʟᴍᴏsᴛ ᴀʟʟ ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴜʀsᴇs, ᴍᴀᴋɪɴɢ ᴛʜᴇ ᴘʀᴏᴄᴇss ᴇғғᴏʀᴛʟᴇss ᴀɴᴅ ᴇғғɪᴄɪᴇɴᴛ.**
+"""
+
+FORCE_MSG = """
+**ʜᴇʏ** {}
+
+**ᴀᴄᴄᴏʀᴅɪɴɢ ᴛᴏ ᴍʏ ᴅᴀᴛᴀʙᴀsᴇ ʏᴏᴜ'ᴠᴇ ɴᴏᴛ ᴊᴏɪɴᴇᴅ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ ʏᴇᴛ, ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴜsᴇ ᴍᴇ ᴛʜᴇɴ ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ ᴀɴᴅ sᴛᴀʀᴛ ᴍᴇ ᴀɢᴀɪɴ !**
+"""
+
+HELP_TEXT = """
+📝 Notice : Most people won't be able to use certain commands because they are premium-only.
+
+`/appx` : For Appx apps [Multi Purpose] [Free]
+`/cp` : For Classplus apps [Multi Purpose] [Free]
+`/pw` : Single Purpose [Paid]
+`/vision` : Single Purpose [Paid]
+`/utkarsh` : Single Purpose [Paid]
+"""
+
+
