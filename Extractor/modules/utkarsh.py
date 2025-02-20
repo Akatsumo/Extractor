68dad2ed7432530d4c42bf1532faa4e3a0fa708b
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-02-20 22:21:20 +0530

    Update utkarsh.py

--- Extractor/modules/utkarsh.py
+++ Extractor/modules/utkarsh.py
@@ -162,10 +162,8 @@ async def utkarsh_login(_, message):
     async with aiohttp.ClientSession() as session:
         async with session.post(login_url, cookies=cookies, data=data) as response:
             if response.status == 200:
-                resp = await response.json()["response"] 
-                t = await response.text()
-                print(t)
-                res = utkarsh_decrypt(resp)
+                resp = await response.text()
+                res = utkarsh_decrypt(json.loads(resp)["response"])
                 token = json.loads(res)["data"]["jwt"]
                 await msg.edit_text("✅ **Login Successfully**")
             else:
