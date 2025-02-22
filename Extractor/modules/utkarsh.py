43a25933f08e1b8be1d7b6f75726a437eef23f78
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-02-22 18:21:56 +0530

    Update utkarsh.py

--- Extractor/modules/utkarsh.py
+++ Extractor/modules/utkarsh.py
@@ -174,11 +174,12 @@ async def utkarsh_login(_, message):
         async with session.post(
             "https://online.utkarsh.com/web/Profile/my_course", cookies=cookies, data=data
         ) as response:
-            data = await response.text()            
+            data = await response.text() 
+            await message.reply_text(data)
+            return
             respon = main_func.utkarsh_decrypt(json.loads(data)["response"])
             data = json.loads(respon)
-            await message.reply_text(data)
-            return 
+             
 #        FFF = "**BATCH ID   -   BATCH NAME**\n\n"
 #        for course in data["data"].get("data"):
 #            FFF += f"**`{course['id']}`   -   {course['title']}**\n\n"
