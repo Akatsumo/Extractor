418c49018d501ab2be6e32fb62791cdcaa939ee4
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-02-23 00:00:48 +0530

    Update start.py

--- Extractor/modules/start.py
+++ Extractor/modules/start.py
@@ -1,6 +1,7 @@
 from Extractor import app
 from pyrogram import filters, enums
 from Extractor.core import script
+fron Extractor.modules import appx
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
@@ -54,6 +55,16 @@ async def handle_callback(_, query):
             script.HELP_TEXT,
             reply_markup=button
         )
+    elif query.data.startswith("appx"):
+        data = query.data.split("_")[1]
+        task = data.split("*")
+        name, api = task[1].split("#")
+        if task[0] == "V2":
+            await query.answer("waito...", show_alert=True)
+            await appx.appex_v3_txt(app, query.message, user_id, api, name)
+        else:
+            await query.answer("waito...", show_alert=True)
+            await appx.appex_v3_txt(app, query.message, user_id, api, name)
 
 
 
