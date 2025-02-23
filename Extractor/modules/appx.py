09d931c398a34af9d724a441810a92db1f3895bf
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-02-23 20:31:18 +0530

    Update appx.py

--- Extractor/modules/appx.py
+++ Extractor/modules/appx.py
@@ -216,6 +216,7 @@ async def course_content(session, api, headers, course_id, parent_id=-1):
     data_list = (await response.json()).get("data", [])
     
     for data in data_list:
+        print(data)
         title = data.get("Title", "Unknown Title")
         material_type = data.get("material_type", "")
         pdf_link = data.get("pdf_link", "")
@@ -288,91 +289,15 @@ async def appex_v2_txt(app, message, user_id, api, name):
             f.write("\n".join(lectures))
         
         caption = f"**App Name** : `{name}`\n**Batch Name** : `{batch_name}`\n\n📜 **Total Materials** : `{len(lectures)}`\n⌚️ **Time Taken** : `{elapsed} sec`"
-        
-        await app.send_document(chat_id=message.chat.id, document=file_name, caption=caption)
+        me = await app.get_me()
+        big_file_id = me.photo.big_file_id
+        thumb = await asyncio.create_task(app.download_media(big_file_id))
+           
+        await app.send_document(chat_id=message.chat.id, document=file_name, caption=caption, thumb=thumb)
         os.remove(file_name)
         await msg.delete()
         await message.reply_text(f"✅ Done\n\n📝**User ID** : `{userid}`\n✏️ **Token** : `{token}`")
 
-
-
-
-
-
-"""
-async def appex_v2_txt(app, message, user_id, api, name):
-    async with aiohttp.ClientSession() as session:
-        raw_url = f"https://{api}/post/userLogin"
-        headers = {
-            "Auth-Key": "appxapi",
-            "User-Id": "-2",
-            "Authorization": "",
-            "User_app_category": "",
-            "Language": "en",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Accept-Encoding": "gzip, deflate",
-            "User-Agent": "okhttp/4.9.1"
-        }
-        
-        msg = await message.reply_text("**🔑 For access, send your ID & Password as: ID*Password**")
-        
-        try:
-            input1 = await app.listen(user_id, timeout=30)
-            raw_text = input1.text
-        except:
-            return await message.reply_text("⏳ Timeout! Please try again.")
-        
-        if "*" in raw_text:
-            email, password = raw_text.split("*")
-        else:
-            return await msg.edit_text("😒 **Incorrect format. Use ID*Password**")
-        
-        await input1.delete()
-        
-        response = await session.post(raw_url, data={"email": email, "password": password}, headers=headers)
-        if response.status != 200:
-            return await msg.edit_text("😒 **Login failed, incorrect credentials.**")
-            
-        output = await response.json()     
-        userid, token = output["data"]["userid"], output["data"]["token"]
-        headers.update({"User-Id": userid, "Authorization": token})
-        await msg.edit_text("✅ **Login Successful**")
-        
-        response = await session.get(f"https://{api}/get/get_all_purchases?userid={userid}&item_type=10", headers=headers)
-        b_data = (await response.json()).get('data', [])
-        
-        batch_list = "**BATCH-ID  -  BATCH NAME**\n\n"
-        batch_map = {}
-        for data in b_data:
-            for cdata in data['coursedt']:
-                batch_list += f"`{cdata['id']}`  -   **{cdata['course_name']}**\n\n"
-                batch_map[cdata['id']] = cdata['course_name']
-        
-        await msg.edit_text(f"{batch_list}\n\n**📊 Now send the Batch ID to Download**")
-        input2 = await app.listen(user_id)
-        course_id = input2.text.strip()
-        await input2.delete()
-        
-        batch_name = batch_map.get(course_id, "Unknown Batch")
-        await msg.edit_text("**Extracting Video Links, Please Wait 📥**")
-        
-        start_time = time.time()
-        links = await course_content(session, api, headers, message, course_id)
-        elapsed = round(time.time() - start_time, 2)
-        
-        file_name = batch_name.replace("/", "")
-        file_path = f"{file_name}_{user_id}.txt"
-        with open(file_path, "w") as f:
-            f.write(links)
-        
-        
-        caption = f"**App Name** : `{name}`\n**Batch Name** : `{batch_name}`\n\n🍿 **Total Videos** : `0`\n📝 **Total Pdf** : `0`\n⌚️ **Time Taken** : `{elapsed}`"     
-        await app.send_document(chat_id=message.chat.id, document=file_path, caption=caption)
-        os.remove(file_path)
-        await msg.delete()
-        await message.reply_text(f"✅ Done\n\n📝**User ID** : `{userid}`\n✏️ **Token** : `{token}`")
-
-"""
 
 # --------------------------- Appex-Command --------------------------- #
 
