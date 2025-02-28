e2df56de00b242eda2bcf732f3a4cc4117d3464b
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-02-28 16:22:59 +0530

    Update classplus.py

--- Extractor/modules/classplus.py
+++ Extractor/modules/classplus.py
@@ -64,10 +64,12 @@ async def verify_otp(session, otp_num, org_id, phone, sessionID):
 
 async def extract_links(session, headers, course_id):
     url = f"https://api.classplusapp.com/v2/course/content/get?courseId={course_id}&folderId=0&storeContentEvent=false"
-    response = await session.post(url, headers)
-    output = await response.json()
-    for content in data.get("data", {}).get("courseContent", []):
-        url = f""
+    response1 = await session.post(url, headers)
+    output1 = await response1.json()
+    for content in output1.get("data", {}).get("courseContent", []):
+        url = f"https://api.classplusapp.com/v2/course/content/get?courseId={course_id}&folderId={content}&storeContentEvent=false"
+        response2 = await session.post(url, headers)
+        output2 = await response2.json()
     
     
 
