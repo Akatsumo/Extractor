f3aa98dc57b831b1f7165199ab4f166b4777ab60
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-03-08 02:43:47 +0530

    Update utkarsh.py

--- Extractor/modules/utkarsh.py
+++ Extractor/modules/utkarsh.py
@@ -182,12 +182,13 @@ async def utkarsh_login(_, message):
         response = await session.post("https://online.utkarsh.com/web/Profile/my_course", cookies=cookies, headers=headers, data=data) 
         output = await response.text()
         respon = main_func.utkarsh_decrypt(json.loads(output)["response"])
-            
+        print(respon)
+        
         FFF = "**BATCH ID   -   BATCH NAME**\n\n"
         for course in respon["data"].get("data"):
             FFF += f"**`{respon['id']}`   -   {respon['title']}**\n\n"
 
-        print(respon)
+        
         await msg.edit_text(f"{FFF}\n\n**📊 Now send the Batch ID to Download**")
         try:
             input2 = await app.listen(user_id, timeout=30)  
