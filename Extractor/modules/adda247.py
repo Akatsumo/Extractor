225f25d45817e386f7a1558bc64d029eff1d300d
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-03-08 19:47:15 +0530

    Update adda247.py

--- Extractor/modules/adda247.py
+++ Extractor/modules/adda247.py
@@ -15,7 +15,7 @@ from Extractor.core.main_func import get_time
 async def csrf_token(session):
   url = "https://userapi.adda247.com/csrf/token?src=aweb"
   response = await session.get(url)
-  data = await resonse.json()
+  data = await response.json()
   if data["success"]:
     return data["data"]
   else:
