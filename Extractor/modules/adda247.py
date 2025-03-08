0e0680645c3568c078f9939804c4c6696e579235
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-03-08 20:59:46 +0530

    Update adda247.py

--- Extractor/modules/adda247.py
+++ Extractor/modules/adda247.py
@@ -165,11 +165,10 @@ async def adda_txt(_, message):
                 input1 = await app.listen(user_id=user_id, timeout=30)
                 if "*" in input1.text:
                     email, password = input1.text.split("*")
-                    print(email)
                     response = await session.post(login_url, data = {"email": email, "providerName": "email", "sec": password}, headers=headers)
                     
-                    if response.status != 200:
-                       return await msg.edit_text("😒 **Login failed, incorrect credentials.**")
+                    #if response.status != 200:
+                    #   return await msg.edit_text("😒 **Login failed, incorrect credentials.**")
 
                     output = await response.json()
                     print(output)
