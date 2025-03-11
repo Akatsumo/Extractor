54e69d2e838e359e44812ffba15a769369b8f1a8
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-03-11 12:15:48 +0530

    Update appx.py

--- Extractor/modules/appx.py
+++ Extractor/modules/appx.py
@@ -81,14 +81,11 @@ async def course_extract(session, api, headers, token, course_id):
                             encrypted_links = output.get("encrypted_links", [])
                             video_path, video_key = None, None
                         
-                            for link in encrypted_links:
-                                if link.get("quality") == "720p":
-                                    try:
-                                        video_path = appx_decrypt(link.get("path", "").split(":")[0])
-                                        video_key = appx_decrypt(link.get("key", "").split(":")[0])
-                                        break
-                                    except Exception as decrypt_error:
-                                        print(f"Error decrypting video for {title}: {decrypt_error}")
+                            try:
+                                video_path = appx_decrypt(encrypted_links[0].get("path", "").split(":")[0])
+                                video_key = appx_decrypt(encrypted_links[0].get("key", "").split(":")[0])         
+                            except Exception as decrypt_error:
+                                print(f"Error decrypting video for {title}: {decrypt_error}")
                         
                             pdf_link = output.get("pdf_link", "")
                             pdf_key = output.get("pdf_encryption_key", "")
