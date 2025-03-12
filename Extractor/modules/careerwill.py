f9646c5d1e4a0eb247956a67f19695fcf4765e23
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-03-12 10:52:49 +0530

    Update careerwill.py

--- Extractor/modules/careerwill.py
+++ Extractor/modules/careerwill.py
@@ -110,12 +110,12 @@ async def adda_txt(_, message):
             await msg.edit_text("✅ **Login Successful**")
           
             response = await session.get(f"https://web.careerwill.com/_next/data/RqQQCO-Y8ngCTaHq8KW2p/live-classes.json?view=List&batch_type=my", cookies=cookies)
-            print(response.json())
-            if response.status != 200:
-              return await msg.edit_text("😒 **Login failed, incorrect credentials.**")
 
-            batch_data = (await response.json())["myBatchData"]
-            
+#            if response.status != 200:
+#              return await msg.edit_text("😒 **Login failed, incorrect credentials.**")
+
+            batch_data = (await response.json()) #["myBatchData"]
+            print(batch_data)
             if not batch_data:
                 return await msg.edit_text("No batch data found.")
             
