2db02a7de6d38336a5149a66350985e30b913e88
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-06-08 11:11:38 +0530

    Update cdsjourney.py

--- Extractor/modules/cdsjourney.py
+++ Extractor/modules/cdsjourney.py
@@ -16,7 +16,8 @@ cookies = {}
 headers = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'en-US,en;q=0.9',
-    'referer': ''   
+    'referer': '',  
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36',
 }
 
 
