16f69ec5258674409e64998fbdd9a2aa58977423
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-09-29 00:24:26 +0530

    Update core_func.py

--- Extractor/core/core_func.py
+++ Extractor/core/core_func.py
@@ -4,26 +4,29 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 appNameDict = {
     "utkarsh_": {"name": "Utkarsh", "func": utkarsh.handle_utk_logic},
-   # "appx_": {"name": "Appx", "func": appx.appx_logins},
-   # "khan_": {"name": "Khan", "func": modules.khan.khan_login},
-   # "careerwill_": {"name": "Careerwill", "func": modules.careerwill.careerwill_login},
-   # "cdsjourney_": {"name": "CdsJourney", "func": modules.cdsjourney.cdsjourney_login},
-   # "adda247_": {"name": "Adda 247", "func": modules.adda247.adda_login},
-   # "classplus_": {"name": "Classplus", "func": modules.classplus.classplus_login},
-   # "vajiram_": {"name": "Vajiram", "func": vajiramias.vajiram_login},
-   # "kpias_": {"name": "kpIAS", "func": modules.kpias.kpias_login},
+    #"appx_": {"name": "Appx", "func": modules.appx.appx_logins},
+    "khan_": {"name": "Khan", "func": modules.khan.khan_login},
+    "careerwill_": {"name": "Careerwill", "func": modules.careerwill.careerwill_login},
+    "cdsjourney_": {"name": "CdsJourney", "func": modules.cdsjourney.cdsjourney_login},
+    "adda247_": {"name": "Adda 247", "func": modules.adda247.adda_login},
+    "classplus_": {"name": "Classplus", "func": modules.classplus.classplus_login},
+    "vajiram_": {"name": "Vajiram", "func": vajiramias.vajiram_login},
+    "kpias_": {"name": "kpIAS", "func": modules.kpias.kpias_login},
 }
 
 PER_PAGE = 15
 
-def get_page(page: int, appNameDict=appNameDict):
+def get_page(page: int, appNameDict=appNameDict, appx: bool = False):
     keys = list(appNameDict.keys())  
     start = page * PER_PAGE
     end = start + PER_PAGE
-    buttons = []
-
     if start >= len(keys):
         return InlineKeyboardMarkup([[InlineKeyboardButton("🚫 No more pages", callback_data="noop")]])
+
+    buttons = []
+
+    if appx:
+        buttons.append([InlineKeyboardButton("🌿 Appx Manual", callback_data="appx_manual")])
 
     row = []
     for i, key in enumerate(keys[start:end], 1):
@@ -33,13 +36,17 @@ def get_page(page: int, appNameDict=appNameDict):
             row = []
     if row:
         buttons.append(row)
-
+        
     nav = []
     if page > 0:
         nav.append(InlineKeyboardButton("＜ ᴘʀᴇᴠ", callback_data=f"page_{page-1}"))
     nav.append(InlineKeyboardButton("↺ ʙ ᴀ ᴄ ᴋ ↻", callback_data="home_"))
-    if end < len(keys):
+    if end < len(keys):  
         nav.append(InlineKeyboardButton("ɴᴇxᴛ ＞", callback_data=f"page_{page+1}"))
-    buttons.append(nav)
+
+    if nav:
+        buttons.append(nav)
 
     return InlineKeyboardMarkup(buttons)
+
+
