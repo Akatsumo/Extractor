ee850e6a493f4f9d355e4ba46da5628d0a59df2f
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-09-28 10:36:35 +0530

    Update core_func.py

--- Extractor/core/core_func.py
+++ Extractor/core/core_func.py
@@ -1,29 +1,18 @@
 from Extractor import module
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-callbackDict = {
-           "utkarsh_" : {"func" : module.utkarsh.handle_utk_logic},
-           "appx_" : {"func" : module.appx.appx_logins},
-           "khan_" : {"func" : module.khan.khan_login},
-           "careerwill_" : {"func" : module.careerwill.careerwill_login},
-           "cdsjourney_" : {"func" : module.cdsjourney.cdsjourney_login},
-           "adda247_" : {"func" : module.adda247.adda_login},
-           "classplus_" : {"func" : mdoule.classplus.classplus_login},
-           "vajiram_" : {"func" : module.vajiram.vajiram_login},
-           "kpias_" : {"func" : module.kpias.kpias_login},
+appNameDict = {
+           "utkarsh_" : {"name": "Utkarsh", "func" : module.utkarsh.handle_utk_logic},
+           "appx_" : {"name": "Appx", "func" : module.appx.appx_logins},
+           "khan_" : {"name": "Khan", "func" : module.khan.khan_login},
+           "careerwill_" : {"name": "Careerwill", "func" : module.careerwill.careerwill_login},
+           "cdsjourney_" : {"name": "CdsJourney", "func" : module.cdsjourney.cdsjourney_login},
+           "adda247_" : {"name": "Adda 247", "func" : module.adda247.adda_login},
+           "classplus_" : {"name": "Classplus", "func" : mdoule.classplus.classplus_login},
+           "vajiram_" : {"name": "Vajiram", "func" : module.vajiram.vajiram_login},
+           "kpias_" : {"name": "kpIAS", "func" : module.kpias.kpias_login},
 }
 
-
-
-
-appName = ["CareerWill", "Utkarsh", "Khan", "appx", "CdsJourney",
-           "CareerWill", "Utkarsh", "Khan", "appx", "CdsJourney",
-           "CareerWill", "Utkarsh", "Khan", "appx", "CdsJourney",
-           "CareerWill", "Utkarsh", "Khan", "appx", "CdsJourney",
-           "CareerWill", "Utkarsh", "Khan", "appx", "CdsJourney",
-           "CareerWill", "Utkarsh", "Khan", "appx", "CdsJourney",
-           "CareerWill", "Utkarsh", "Khan", "appx", "CdsJourney",
-           "CareerWill", "Utkarsh", "Khan", "appx", "CdsJourney",]
 
 PER_PAGE = 15
 
@@ -32,10 +21,10 @@ def get_page(page: int):
     end = start + PER_PAGE
     buttons = []
 
-    if start >= len(appName):
+    if start >= len(appNameDict):
         return [[InlineKeyboardButton("🚫 No more pages", callback_data="noop")]]
 
-    data = appName[start:end]
+    data = appNameDict[start:end]
     row = []
     for i, d in enumerate(data, 1):
         row.append(InlineKeyboardButton(d, callback_data=f"item_{d}"))
@@ -49,7 +38,7 @@ def get_page(page: int):
     if page > 0:
         nav.append(InlineKeyboardButton("⬅️ Prev", callback_data=f"page_{page-1}"))
     nav.append(InlineKeyboardButton("🔙 Back", callback_data="home_"))
-    if end < len(appName):
+    if end < len(appNameDict):
         nav.append(InlineKeyboardButton("➡️ Next", callback_data=f"page_{page+1}"))
     buttons.append(nav)
 
