1ee3449c10a14f67ab3b2d60c3de026f11e55e26
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-09-28 10:20:58 +0530

    Update adda247.py

--- Extractor/modules/adda247.py
+++ Extractor/modules/adda247.py
@@ -217,7 +217,7 @@ async def direct_links(session, headers, course_id):
 
 # ----------------------- Adda-Command ----------------------- #
 
-@app.on_message(filters.command("adda"))
+#@app.on_message(filters.command("adda"))
 async def adda_txt(_, message):
     user_id = message.from_user.id
     try:
