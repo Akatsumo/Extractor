ebcd8a6cf3f6d186a5b84bf8d0a5e1d12d29ebb5
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-09-28 23:37:31 +0530

    Update core_func.py

--- Extractor/core/core_func.py
+++ Extractor/core/core_func.py
@@ -1,17 +1,17 @@
 from Extractor import modules
-from Extractor.modules import vajiramias, utkarsh
+from Extractor.modules import vajiramias, utkarsh, appx
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 appNameDict = {
     "utkarsh_": {"name": "Utkarsh", "func": utkarsh.handle_utk_logic},
     "appx_": {"name": "Appx", "func": modules.appx.appx_logins},
-    "khan_": {"name": "Khan", "func": modules.khan.khan_login},
-    "careerwill_": {"name": "Careerwill", "func": modules.careerwill.careerwill_login},
-    "cdsjourney_": {"name": "CdsJourney", "func": modules.cdsjourney.cdsjourney_login},
-    "adda247_": {"name": "Adda 247", "func": modules.adda247.adda_login},
-    "classplus_": {"name": "Classplus", "func": modules.classplus.classplus_login},
-    "vajiram_": {"name": "Vajiram", "func": vajiramias.vajiram_login},
-    "kpias_": {"name": "kpIAS", "func": modules.kpias.kpias_login},
+   # "khan_": {"name": "Khan", "func": modules.khan.khan_login},
+   # "careerwill_": {"name": "Careerwill", "func": modules.careerwill.careerwill_login},
+   # "cdsjourney_": {"name": "CdsJourney", "func": modules.cdsjourney.cdsjourney_login},
+   # "adda247_": {"name": "Adda 247", "func": modules.adda247.adda_login},
+   # "classplus_": {"name": "Classplus", "func": modules.classplus.classplus_login},
+   # "vajiram_": {"name": "Vajiram", "func": vajiramias.vajiram_login},
+   # "kpias_": {"name": "kpIAS", "func": modules.kpias.kpias_login},
 }
 
 PER_PAGE = 15
