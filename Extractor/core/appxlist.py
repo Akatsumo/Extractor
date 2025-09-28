edcb4c605715b2cbf7934ed12cd039c379ff24e6
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-09-28 19:48:05 +0530

    Update appxlist.py

--- Extractor/core/appxlist.py
+++ Extractor/core/appxlist.py
@@ -1,9 +1,6 @@
+import string
+
 a_to_zList = {
-  "A": "appxSection_A", "B": "appxSection_B", "C": "appxSection_C", "D": "appxSection_D",
-  "E": "appxSection_E", "F": "appxSection_F", "G": "appxSection_G", "H": "appxSection_H",
-  "I": "appxSection_I", "J": "appxSection_J", "K": "appxSection_K", "L": "appxSection_L",
-  "M": "appxSection_M", "N": "appxSection_N", "O": "appxSection_O", "P": "appxSection_P",
-  "Q": "appxSection_Q", "R": "appxSection_R", "S": "appxSection_S", "T": "appxSection_T",
-  "U": "appxSection_U", "V": "appxSection_V", "W": "appxSection_W", "X": "appxSection_X",
-  "Y": "appxSection_Y", "Z": "appxSection_Z",
-            }
+    f"appxSection_{letter}": {"name": letter}
+    for letter in string.ascii_uppercase
+}
