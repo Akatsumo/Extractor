2d9614548fa4676fbec01a74d9ee52e94affa2f8
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-03 08:45:01 +0530

    Update core_func.py

--- Extractor/core/core_func.py
+++ Extractor/core/core_func.py
@@ -1,4 +1,4 @@
-from Extractor.modules import vajiramias, utkarsh, appx, khan, careerwill
+from Extractor.modules import vajiramias, utkarsh, appx, khan, careerwill, adda247, kpias
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 appNameDict = {
@@ -7,9 +7,9 @@ appNameDict = {
     "khan_": {"name": "Khan", "func": khan.khan_login},
     "careerwill_": {"name": "Careerwill", "func": careerwill.careerwill_login},
     #"cdsjourney_": {"name": "CdsJourney", "func": modules.cdsjourney.cdsjourney_login},
-    #"adda247_": {"name": "Adda 247", "func": modules.adda247.adda_login},
+    "adda247_": {"name": "Adda 247", "func": adda247.adda_login},
     #"classplus_": {"name": "Classplus", "func": modules.classplus.classplus_login},
     #"vajiram_": {"name": "Vajiram", "func": vajiramias.vajiram_login},
-    #"kpias_": {"name": "kpIAS", "func": modules.kpias.kpias_login},
+    "kpias_": {"name": "kpIAS", "func": kpias.kpias_login},
 }
 
