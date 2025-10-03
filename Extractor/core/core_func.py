344cde127be563b72db3902c3159afcca322fdde
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-03 08:49:59 +0530

    Update core_func.py

--- Extractor/core/core_func.py
+++ Extractor/core/core_func.py
@@ -3,7 +3,6 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 appNameDict = {
     "utkarsh_": {"name": "Utkarsh", "func": utkarsh.handle_utk_logic},
-    #"appx_": {"name": "Appx", "func": appx.appx_logins},
     "khan_": {"name": "Khan", "func": khan.khan_login},
     "careerwill_": {"name": "Careerwill", "func": careerwill.careerwill_login},
     #"cdsjourney_": {"name": "CdsJourney", "func": modules.cdsjourney.cdsjourney_login},
