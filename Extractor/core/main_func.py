f0b2f8b091767d235c726bec34b268164e91071f
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-09 14:28:28 +0530

    Update main_func.py

--- Extractor/core/main_func.py
+++ Extractor/core/main_func.py
@@ -84,7 +84,7 @@ def decode_base64(encoded_data):
 allDics = {}
 PER_PAGE = 15
 
-def get_page(page: int, appNameDict, DictID="Dic1", back_data="home_", query=None, appx=False page_row=3):
+def get_page(page: int, appNameDict, DictID="Dic1", back_data="home_", query=None, appx=False, page_row=3):
     keys = list(appNameDict.keys())  
     start = page * PER_PAGE
     end = start + PER_PAGE
