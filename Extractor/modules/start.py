02e68477661b4ee6898d003b378bb3a4123dea34
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-09 13:13:28 +0530

    Update start.py

--- Extractor/modules/start.py
+++ Extractor/modules/start.py
@@ -82,6 +82,8 @@ async def handle_callback(_, query):
             await appx.appex_v3_txt(app, query.message, user_id, api, name)
             
     elif query.data.startswith("page_"):
+        if not allDics:
+            return await query.answer("Time up!", show_alert=True)
         page = int(query.data.split("_")[2])
         DictID = query.data.split("_")[1]
         appNameDict = main_func.allDics[DictID]
