b7337638ceb728fbe947bc9c9af888e16ce202d8
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-13 15:41:34 +0530

    Update core_func.py

--- Extractor/core/core_func.py
+++ Extractor/core/core_func.py
@@ -10,7 +10,7 @@ appNameDict = {
     "classplus_": {"name": "Classplus", "func": classplus.classplus_login},
     "vajiram_": {"name": "Vajiram", "func": vajiramias.vajiram_login},
     "kpias_": {"name": "kpIAS", "func": kpias.kpias_login},
-    "videocrypt_": {"name": "Videocrypt", "func": videocrypt.videocryptButtons},
+    "videocrypt_": {"name": "Videocrypt", "func": videocrypt.videocrypt_login},
 }
 
 videoCryptDict = {
