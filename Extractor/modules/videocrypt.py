e288857f2d97e7cff95a156cb7506d2bae4a3d97
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-13 14:26:34 +0530

    Update videocrypt.py

--- Extractor/modules/videocrypt.py
+++ Extractor/modules/videocrypt.py
@@ -5,7 +5,8 @@ import requests
 from Extractor import app
 from pyrogram import filters
 from base64 import b64decode
-from Extractor.core import main_func
+from Extractor.core import main_func, script
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 def header_definer(name):
@@ -219,3 +220,7 @@ class VideoCryptExtractor:
         except Exception as e:
             await message.reply_text(f"Error: `{str(e)}`")
         
+asyn def videoDecryptButtons():
+    buttons = await main_func.get_page(0, main_func.videoDecryptDict, DictID="VideoDecrypt1", back_data="tools_")
+    await message.edit_text(script.TOOLS_TEXT, reply_markup=buttons)
+    
