6827de443e0ffc3a92304ee4adbf99aef19db470
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-14 17:46:24 +0530

    Update careerwill.py

--- Extractor/modules/careerwill.py
+++ Extractor/modules/careerwill.py
@@ -176,5 +176,5 @@ class CareerwillExtractor:
 extractor = CareerwillExtractor()
 
 @app.on_message(filters.command("cw"))
-async def careerwill_command(client, message):
-    await extractor.login_and_extract(client, message)
+async def careerwill_command(_, message, user_id):
+    await extractor.start_login(_, message, user_id)
