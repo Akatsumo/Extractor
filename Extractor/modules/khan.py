7f0703d4dc0ee17144abbd73e419d8ab84505f6a
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-14 14:29:02 +0530

    Update khan.py

--- Extractor/modules/khan.py
+++ Extractor/modules/khan.py
@@ -37,88 +37,93 @@ class KhanExtractor:
             print(f"Error: {e}")
             return []
 
-    async def login(self, session, msg, user_id):
-        await msg.edit_text(
-            "**🔑 For access, please transmit your ID & Password in the correct sequence:\n\n🔒 Send like this: ID*Password**"
-        )
-        try:
-            input1 = await app.listen(user_id=user_id, timeout=30)
-            creds = input1.text.strip()
-            if "*" in creds:
-                phone, password = creds.split("*", 1)
-                async with session.post(self.login_url, json={"phone": phone, "password": password}) as response:
-                    if response.status != 200:
-                        await msg.edit_text("😒 **Login failed, incorrect credentials.**")
-                        return None
-                    output = await response.json()
-                    token = output.get("token", "")
-            else:
-                token = creds
-            await input1.delete()
-            await msg.edit_text("✅ **Login Successful**")
-            return token
-        except asyncio.TimeoutError:
-            await msg.edit_text("⏳ Timeout! Please try again.")
-            return None
-        except Exception as e:
-            await msg.edit_text(f"⚠️ Error while login: `{e}`")
-            return None
 
     async def get_batches(self, session, headers):
         async with session.get(self.paid_courses_url, headers=headers) as response:
             output = await response.json()
         batch_data = output if isinstance(output, list) else output.get("data", [])
         batch_map = {}
-        batch_list = "**BATCH-ID  -  BATCH NAME**\n\n"
+        batch_courses = "**BATCH-ID  -  BATCH NAME**\n\n"
         for data in batch_data:
             batch_id = str(data.get("id"))
             batch_name = data.get("title", "No Title")
             slug = data.get("slug", "")
-            batch_list += f"`{batch_id}`  -   **{batch_name}**\n\n"
+            batch_courses += f"`{batch_id}`  -   **{batch_name}**\n\n"
             batch_map[batch_id] = {"name": batch_name, "slug": slug}
-        return batch_list, batch_map
+        return batch_courses, batch_map
+
+
+    async def start_login(self, message, user_id):
+        user_id = user_id or message.from_user.id
+        try:
+            async with aiohttp.ClientSession() as session:
+                msg = await message.reply_text("🔑 Enter login credentials (Mobile*Password or Token): ")
+                input1 = await app.listen(user_id=user_id, timeout=30)
+                creds = input1.text.strip()
+                await input1.delete()
+            
+               if "*" in creds:
+                   phone, password = creds.split("*", 1)
+                   async with session.post(self.login_url, json={"phone": phone, "password": password}) as response:
+                       if response.status != 200:
+                           return await msg.edit_text("😒 **Login failed, incorrect credentials.**")
+                       output = await response.json()
+                       token = output.get("token")
+               else:
+                   token = creds
+               headers = {"Authorization": f"Bearer {token}", "Accept": "application/json",}
+               batch_courses, batch_map = await self.get_batches(session, headers)
+               await msg.edit_text(f"{batch_courses}\n\n**📊 Now send the Batch ID to Download**")
+               input2 = await app.listen(user_id=user_id)
+               batch_id = input2.text.strip()
+               await input2.delete()
+               batch_info = batch_map.get(course_id)
+            
+               if not batch_info:
+                   return await msg.edit_text(" Invalid Batch ID. Please try again.")
+                   
+               slug = batch_info["slug"]
+               batch_name = batch_info["name"]
+               await msg.edit_text(f"**Extracting Course Content for `{batch_name}` Please Wait 📥**")
+            
+               start_time = time.time()
+               lectures = await asyncio.create_task(self.extract_lessons(session, headers, slug))
+               end_time = time.time()
+               elapsed = get_time(end_time - start_time)
+            
+               file_name = f"{batch_name.replace('/', '')}_{user_id}.txt"
+               with open(file_name, "w", encoding="utf-8") as f:
+                   f.write("\n".join(lectures[::-1]))
+                   
+               caption = f"**Batch Name** : `{batch_name}`\n\n📜 **Total Materials** : `{len(lectures)}`\n⌚️ **Time Taken** : `{elapsed} sec`"
+               me = await app.get_me()
+               big_file_id = me.photo.big_file_id
+               thumb = await asyncio.create_task(app.download_media(big_file_id))
+
+               await app.send_document(chat_id=message.chat.id, document=file_name, caption=caption, thumb=thumb)
+               os.remove(file_name)
+               await msg.delete()
+               await message.reply_text(f"✅ Done\n\n✏️ **Token** : `{token}`")
+    
+        except Exception as e:
+            await message.reply_text(f"Error: `{e}`")
+          
+
+
+
+
+
+
+
+
+
 
 
 @app.on_message(filters.command("khan"))
 async def khan_handler(_, message, user_id=None):
     extractor = KhanExtractor()
-    user_id = user_id or message.from_user.id
-    try:
-        async with aiohttp.ClientSession() as session:
-            msg = await message.reply_text("🔐 **Initializing Login...**")
-            token = await extractor.login(session, msg, user_id)
-            if not token:
-                return
-            headers = {
-                "Authorization": f"Bearer {token}",
-                "Accept": "application/json",
-            }
-            batch_list, batch_map = await extractor.get_batches(session, headers)
-            await msg.edit_text(f"{batch_list}\n\n**📊 Now send the Batch ID to Download**")
-            input2 = await app.listen(user_id=user_id)
-            course_id = input2.text.strip()
-            await input2.delete()
-            batch_info = batch_map.get(course_id)
-            if not batch_info:
-                return await msg.edit_text("❌ Invalid Batch ID. Please try again.")
-            slug = batch_info["slug"]
-            batch_name = batch_info["name"]
-            await msg.edit_text(f"**Extracting Course Content for `{batch_name}` Please Wait 📥**")
-            start_time = time.time()
-            lectures = await asyncio.create_task(extractor.extract_lessons(session, headers, slug))
-            end_time = time.time()
-            elapsed = get_time(end_time - start_time)
-            file_name = f"{batch_name.replace('/', '')}_{user_id}.txt"
-            with open(file_name, "w", encoding="utf-8") as f:
-                f.write("\n".join(lectures[::-1]))
-            caption = (
-                f"**Batch Name** : `{batch_name}`\n\n"
-                f"📜 **Total Materials** : `{len(lectures)}`\n"
-                f"⌚️ **Time Taken** : `{elapsed} sec`"
-            )
-            await app.send_document(chat_id=message.chat.id, document=file_name, caption=caption)
-            os.remove(file_name)
-            await msg.delete()
-            await message.reply_text(f"✅ Done\n\n✏️ **Token** : `{token}`")
-    except Exception as e:
-        await message.reply_text(f"Error: `{str(e)}`")
+    extractor.start_login(_, message, user_id)
+    
+    
+           
+            
