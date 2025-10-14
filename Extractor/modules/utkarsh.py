27ecf74d9f52619f2bd4a3451ca206fa8c18ee7c
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-14 20:48:05 +0530

    Update utkarsh.py

--- Extractor/modules/utkarsh.py
+++ Extractor/modules/utkarsh.py
@@ -26,8 +26,8 @@ class UtkarshExtractor:
             'user-agent': 'okhttp/4.11.0',
         }
         self.LOGIN_DATA = {
-            "device_id": "e0d97edefabd9635", #gen_device_id(),
-            "device_token": "f9c8b1a4d7e34c1aa6e89f17e5b2d3c76a5e8d9f12a34b6c8d0e1f2a3b4c5d6e", #"utkarsh_device",
+            "device_id": gen_device_id(),
+            "device_token": "utkarsh_device",
             "is_social": "0",
         }
 
