138d00ad96cfc5e7137e6a851d8b4da4099bac2e
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-14 14:49:12 +0530

    Update khan.py

--- Extractor/modules/khan.py
+++ Extractor/modules/khan.py
@@ -11,7 +11,7 @@ class KhanExtractor:
     def __init__(self):
         self.login_url = "https://api.khanglobalstudies.com/cms/login"
         self.paid_courses_url = "https://api.khanglobalstudies.com/v1/courses/paid"
-        self.lessons_url = f"https://api.khanglobalstudies.com/cms/user/courses/{slug}/lessons"
+        self.lessons_url = "https://api.khanglobalstudies.com/cms/user/courses/{slug}/lessons"
 
     async def extract_lessons(self, session, headers, slug):
         try:
@@ -87,7 +87,6 @@ class KhanExtractor:
 
                 start_time = time.time()
                 lectures = await self.extract_lessons(session, headers, slug)
-                print(lectures)
                 end_time = time.time()
                 elapsed = get_time(end_time - start_time)
 
