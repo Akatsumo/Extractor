b464ca78e92abfc1eb967dc53115133e9f3b15f5
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-15 19:00:27 +0530

    Update main_func.py

--- Extractor/core/main_func.py
+++ Extractor/core/main_func.py
@@ -2,11 +2,37 @@ import time
 import base64
 import json
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import unpad, pad
+from config import CHANNEL_ID
+from FileSwoop.core import script
 from base64 import b64decode, b64encode
+from Crypto.Util.Padding import unpad, pad
+from pyrogram.errors import UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
+
+# --------------------------------------------------------------------------- #
+
+async def gen_link(app,chat_id):
+   link = await app.export_chat_invite_link(chat_id)
+   return link
+  
+
+async def subscribe(app, message):
+   update_channel = CHANNEL_ID
+   url = await gen_link(app, update_channel)
+   if update_channel:
+      try:
+         user = await app.get_chat_member(update_channel, message.from_user.id)
+         if user.status == "kicked":
+            await message.reply_text("Sorry Sir, You are Banned. Contact My Support Group @DiabloForge")
+            return 1
+      except UserNotParticipant:
+         await message.reply_photo(photo="https://telegra.ph/file/b7a933f423c153f866699.jpg",caption=script.FORCE_MSG.format(message.from_user.mention), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🤖 ᴊᴏɪɴ ᴜᴘᴅᴀᴛᴇs ᴄʜᴀɴɴᴇʟ 🤖", url=f"{url}")]]))
+         return 1
+      except Exception:
+         await message.reply_text("Something Went Wrong. Contact My Support Group")
+         return 1
 
 # --------------------------------------------------------------------------- #
 
