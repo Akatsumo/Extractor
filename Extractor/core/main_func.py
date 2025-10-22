18c37bd4ff561ab7aa2320463a99afc951d0404b
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-22 11:33:50 +0530

    Update main_func.py

--- Extractor/core/main_func.py
+++ Extractor/core/main_func.py
@@ -1,4 +1,4 @@
-import time
+import time, os
 import base64
 import json, asyncio
 from Crypto.Cipher import AES
@@ -15,6 +15,7 @@ async def send_file(app, file_name, user_id, caption, onlyThumb=False):
     me = await app.get_me()
     thumb = await asyncio.create_task(app.download_media(me.photo.big_file_id)) if me.photo else None
     if onlyThumb:
+        os.remove(thumb)
         return thumb
     msg = await app.send_document(chat_id=user_id, document=file_name, caption=caption, thumb=thumb)
     if LOGS_CHANNEL:
@@ -24,7 +25,8 @@ async def send_file(app, file_name, user_id, caption, onlyThumb=False):
         except Exception as e:
             print(f"Failed to send message to log channel: {e}")
             pass
-        
+    os.remove(file_name)
+    os.remove(thumb)    
     return True
    
 # --------------------------------------------------------------------------- #
