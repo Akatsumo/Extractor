244a77fb9b35b9fe13918dbbd35b80f0ad843070
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-22 11:01:59 +0530

    Update studyiq_free.py

--- Extractor/modules/studyiq_free.py
+++ Extractor/modules/studyiq_free.py
@@ -1,8 +1,8 @@
-import os
+import os, time
 import asyncio
-import requests
-import time
+import requests 
 from Extractor import app
+from pyrogram.enums import ParseMode
 from Extractor.core import main_func
 from pyromod.exceptions import ListenerTimeout
 
@@ -65,22 +65,27 @@ async def studyiq_access(_, message, user_id=None):
         if not data:
             return await msg.edit_text("No course data found.")
 
-        batch_list = "📚 **Available Batches:**\n\n"
+        batch_list = "📚 <b>Available Batches:</b>\n\n"
         for course in data:
-            batch_list += f"`{course.get('course_id')}` - **{course.get('course_title')}**\n"
-
+            batch_list += f"<code>{course.get('course_id')}</code> - <b>{course.get('course_title')}</b>\n"
+            
+        me = await app.get_me()
+        thumb = await app.download_media(me.photo.big_file_id) if me.photo else None
+        caption = "</b>📊 Now send the Batch ID to Download</b>"
+        
         if len(batch_list) > 4000:
             batch_list_name = f"{keyword_str}_batchList_{user_id}.txt"
             with open(batch_list_name, "w", encoding="utf-8") as f:
                 f.write(batch_list)
-            await app.send_document(chat_id=user_id, document=batch_list_name)
-            await msg.edit_text("**📊 Now send the Batch ID to Download**")
+            batch_file = await app.send_document(chat_id=user_id, document=batch_list_name, caption=caption, thumb=thumb)
+            await msg.edit_text()
             os.remove(batch_list_name)
         else:
-            await msg.edit_text(f"{batch_list}\n**📊 Now send the Batch ID to Download**")
+            await msg.edit_text(f"{batch_list}\n{caption}")
 
         input2 = await app.listen(user_id=user_id, timeout=30)
         batch_id = input2.text.strip()
+        await batch_file.delete()
         await input2.delete()
 
         batch_name = next((course["course_title"] for course in data if str(course["course_id"]) == batch_id), None)
@@ -108,11 +113,9 @@ async def studyiq_access(_, message, user_id=None):
             f"🍿 **Videos** : {v_count} | 📝 **PDFs** : {p_count}\n"
             f"⌚️ **Time Taken** : `{elapsed}`"
         )
-
-        me = await app.get_me()
-        thumb = await app.download_media(me.photo.big_file_id) if me.photo else None
         await app.send_document(chat_id=message.chat.id, document=file_name, caption=caption, thumb=thumb)
         os.remove(file_name)
+        os.remove(thumb)
         await msg.delete()
 
     except ListenerTimeout:
