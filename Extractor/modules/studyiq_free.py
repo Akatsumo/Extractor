bd207100ad77e5fc4af75251e70bcad1cfd3bf4c
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-22 11:39:28 +0530

    Update studyiq_free.py

--- Extractor/modules/studyiq_free.py
+++ Extractor/modules/studyiq_free.py
@@ -111,7 +111,7 @@ async def studyiq_access(_, message, user_id=None):
             f"🍿 **Videos** : {v_count} | 📝 **PDFs** : {p_count}\n"
             f"⌚️ **Time Taken** : `{elapsed}`"
         )
-        await main_func.send_file(app, file_name, user_id, caption, thumb))
+        await main_func.send_file(app, file_name, user_id, caption, thumb)
         await msg.delete()
 
     except ListenerTimeout:
