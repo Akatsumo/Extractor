0b90f8342cbd656f35ae30d2db47ad9c2d055b11
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-22 11:36:31 +0530

    Update main_func.py

--- Extractor/core/main_func.py
+++ Extractor/core/main_func.py
@@ -11,9 +11,10 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 # --------------------------------------------------------------------------- #
 
-async def send_file(app, file_name, user_id, caption, onlyThumb=False):
-    me = await app.get_me()
-    thumb = await asyncio.create_task(app.download_media(me.photo.big_file_id)) if me.photo else None
+async def send_file(app, file_name, user_id, caption, thumb=None, onlyThumb=False):
+    if not thumb:
+        me = await app.get_me()
+        thumb = await asyncio.create_task(app.download_media(me.photo.big_file_id)) if me.photo else None
     if onlyThumb:
         return thumb
     msg = await app.send_document(chat_id=user_id, document=file_name, caption=caption, thumb=thumb)
