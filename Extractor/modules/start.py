6d3b9579ab732f6c8fbcbaceb64af4026bd44902
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-23 18:34:07 +0530

    Update start.py

--- Extractor/modules/start.py
+++ Extractor/modules/start.py
@@ -80,7 +80,7 @@ async def handle_callback(_, query):
         )
     elif query.data.startswith("autoCallback#"):
         data = query.data.split("#")[1]
-        if data in core_func.appNameDict or data in appxmethod.appxapis or data in core_func.videoCryptDict:
+        if data in core_func.appNameDict or data in appxmethod.appxapis or data in core_func.videoCryptDict or data in core_func.WithoutAppDict:
             if data in appxmethod.appxapis:
                 name = appxmethod.appxapis[f"{data}"]["name"]
                 await query.answer(f"You clicked {name}", show_alert=True)
@@ -92,6 +92,11 @@ async def handle_callback(_, query):
                 await query.answer(f"You clicked {name}", show_alert=True)
                 vdocrypt = core_func.videoCryptDict[f"{data}"]["func"].VideoCryptExtractor(name)
                 await vdocrypt.start_login(_, query.message, user_id)
+                
+            elif data in core_func.WithoutAppDict:
+                name = core_func.WithoutAppDict[f"{data}"]["name"]
+                await query.answer(f"You clicked {name}", show_alert=True)
+                await core_func.WithoutAppDict[f"{data}"]["func"](_, query.message, user_id)
                 
             else:
                 await query.answer(f"You clicked {core_func.appNameDict[f"{data}"]["name"]}", show_alert=True)
