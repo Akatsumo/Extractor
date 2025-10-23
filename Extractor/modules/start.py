69da35f519cbf7595f60800c4a641abd4868b464
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-23 21:43:34 +0530

    Update start.py

--- Extractor/modules/start.py
+++ Extractor/modules/start.py
@@ -61,7 +61,7 @@ async def handle_callback(_, query):
         )
     elif query.data == "tools_":
         await query.message.edit_text(
-            script.TOOLS_TEXT,
+            "wew",
             reply_markup=main_func.get_page(0, core_func.appNameDict, "CoursesName")
         )
     elif query.data == "manual_login":
