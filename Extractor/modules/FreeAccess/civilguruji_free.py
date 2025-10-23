87401cda4c0c13131132a52cc4b502e223574ac4
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-23 08:55:55 +0530

    Update civilguruji_free.py

--- Extractor/modules/FreeAccess/civilguruji_free.py
+++ Extractor/modules/FreeAccess/civilguruji_free.py
@@ -31,7 +31,7 @@ async def course_content(session, batch_id, msg):
             sub_name = sub.get("name", "No Title")
             video_url = sub.get("videoUrl", "No Video URL")
             v_count += 1
-            lecture.append(f"{module_name}|{sub_name}: {clean_video_url(video_url)}")
+            lectures.append(f"{module_name}|{sub_name}: {clean_video_url(video_url)}")
   
     return lectures, v_count, p_count
 
@@ -40,6 +40,7 @@ async def civilguriji_access(_, message, user_id=None):
     session = requests.Session()
 
     try:
+        msg = await message.reply_text("Fetching Civil Guruji All Batches, Please Wait... ")
         url = "https://civilguruji.com/api/course/landing-page-courses"
         headers = {
             "Accept": "application/json, text/plain, */*",
@@ -84,14 +85,13 @@ async def civilguriji_access(_, message, user_id=None):
 
         batch_name = batch_index.get(batch_id)
         if not batch_name:
-            return await message.reply_text("**❌ Invalid Batch ID. Please try again.**")
+            return await message.reply_text("**Invalid Batch ID. Please try again.**")
 
-        msg = await message.reply_text(f"**Extracting Course Content for:** `{batch_name}`\n📥 Please wait...")
+        msg = await message.reply_text(f"**Extracting Course Content, Please Wait 📥**")
 
         start_time = time.time()
         lectures, v_count, p_count = await asyncio.create_task(course_content(session, batch_id, msg))
         end_time = time.time()
-
         if not lectures:
             return await msg.edit_text("No Batch Content found.")
 
@@ -115,3 +115,7 @@ async def civilguriji_access(_, message, user_id=None):
         await message.reply_text("⏰ You didn’t reply in time. Please try again.")
     except Exception as e:
         await message.reply_text(f"⚠️ Error: `{e}`")
+
+
+
+
