9950785dca839b9cf1fd09373b455843af68cd03
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-23 10:41:55 +0530

    Update cdsjourney_free.py

--- Extractor/modules/cdsjourney_free.py
+++ Extractor/modules/cdsjourney_free.py
@@ -76,7 +76,7 @@ async def cdsjourney_access(_, message, user_id=None):
 
             response = session.post("https://www.cdsjourney.com/api/verify_otp/", data={"email": email, "otp": otp})
             if response.status_code != 200:
-                return await msg.edit_text("❌ OTP verification failed, Try again!")
+                return await msg.edit_text("OTP verification failed, Try again!")
 
             token = response.json().get("access_token")
             await message.reply(f"✅ Login successful.\n\nYour token: `{token}`")
@@ -97,7 +97,7 @@ async def cdsjourney_access(_, message, user_id=None):
             batch_file = await app.send_document(chat_id=user_id, document=batch_list_name, caption=caption, thumb=thumb)
             os.remove(batch_list_name)
         else:
-            batch_file = await msg.edit_text(f"{batch_list}\n\n{caption}")
+            await msg.edit_text(f"{batch_list}\n\n{caption}")
 
         input3 = await app.listen(user_id=user_id, timeout=30)
         batch_id = input3.text.strip()
