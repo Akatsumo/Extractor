2e801afd6cee77e049ec869e87a6a6e1e7861842
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-23 21:33:15 +0530

    Update start.py

--- Extractor/modules/start.py
+++ Extractor/modules/start.py
@@ -12,7 +12,7 @@ core_func.appNameDict["appx_"] = {"name": "Appx", "func": appx.appx_logins}
 buttons = InlineKeyboardMarkup([
     [
         InlineKeyboardButton("🧰 Tools", callback_data="tools_"),
-        InlineKeyboardButton("Contact ☎️", user_id=int("8062189975"))
+        InlineKeyboardButton("Contact ☎️", url="https://t.me/Vivan_Official"))
     ]])
 
 button = InlineKeyboardMarkup([
