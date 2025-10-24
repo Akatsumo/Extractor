45fb35bb6aaae30bb4b754b8bffd00e1c3d47bd2
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-24 13:50:15 +0530

    Update qualityEducation_free.py

--- Extractor/modules/FreeAccess/qualityEducation_free.py
+++ Extractor/modules/FreeAccess/qualityEducation_free.py
@@ -1,3 +1,4 @@
+
 import os
 import time
 import asyncio
@@ -104,7 +105,7 @@ async def qualityEducation_access(_, message, user_id=None):
 
         await msg.edit_text("📥 Extracting Course Content, please wait...")
         start_time = time.time()
-        lectures, v_count, p_count = await asyncio.create_task(course_content, session, headers, batch_id, msg)
+        lectures, v_count, p_count = await asyncio.create_task(course_content(session, headers, batch_id, msg))
         end_time = time.time()
 
         if not lectures:
@@ -130,5 +131,6 @@ async def qualityEducation_access(_, message, user_id=None):
         await message.reply_text("⏰ You didn’t reply in time. Please try again.")
     except Exception as e:
         await message.reply_text(f"Error: `{str(e)}`")
+
 
 
