c4c1cd3766875e9e760f64f9a58e5b215a11da1c
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-24 18:29:51 +0530

    Update core_func.py

--- Extractor/core/core_func.py
+++ Extractor/core/core_func.py
@@ -1,5 +1,5 @@
 from Extractor.modules import khan, adda247, classplus, vajiramias, videocrypt, kpias, careerwill, cdsjourney
-from Extractor.modules import cdsjourney_free, classplus_free, civilguruji_free, studyiq_free, qualityEducation_free
+from Extractor.modules import cdsjourney_free, classplus_free, civilguruji_free, studyiq_free, qualityEducation_free, testpaper_free
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 appNameDict = {
@@ -20,6 +20,7 @@ WithoutAppDict = {
     "wcivilguruji_": {"name": "Civil Guruji", "func": civilguruji_free.civilguruji_access},
     "wstudyiq_": {"name": "Study IQ", "func": studyiq_free.studyiq_access},
     "wqualityEducation_": {"name": "Quality Education", "func": qualityEducation_free.qualityEducation_access},
+    "wtestpaper_": {"name": "Test Paper", "func": testpaper_free.testpaper_access},
 }
 
 videoCryptDict = {
