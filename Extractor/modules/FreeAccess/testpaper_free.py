0fa8ed0bb5966456b73127baf16f18652a30dfd1
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-24 18:25:55 +0530

    Update testpaper_free.py

--- Extractor/modules/FreeAccess/testpaper_free.py
+++ Extractor/modules/FreeAccess/testpaper_free.py
@@ -6,7 +6,7 @@ from Extractor.core import main_func
 from pyromod.exceptions import ListenerTimeout
 
 
-async def course_content(session, batch_id, msg):
+async def course_content(session, headers, batch_id, msg):
     lectures, v_count, p_count = [], 0, 0
     
     response_data = session.post("https://testpaperlive.com/api/app/v2/package_chapter_list", data=f"package_id={batch_id}", headers=headers)
@@ -112,7 +112,7 @@ async def testpaper_access(_, message, user_id=None):
         await msg.edit_text("**Extracting Course Content, Please Wait 📥**")
       
         start_time = time.time()
-        lectures, v_count, p_count = await asyncio.create_task(course_content(session, batch_id, msg))
+        lectures, v_count, p_count = await asyncio.create_task(course_content(session, headers, batch_id, msg))
         end_time = time.time()
 
         if not lectures:
