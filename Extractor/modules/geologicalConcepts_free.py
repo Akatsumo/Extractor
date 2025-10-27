d736ac5730136baaeedfe52e6c8d05c1b7025579
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-27 13:23:54 +0530

    Update geologicalConcepts_free.py

--- Extractor/modules/geologicalConcepts_free.py
+++ Extractor/modules/geologicalConcepts_free.py
@@ -18,12 +18,12 @@ async def course_content(session, batch_id, msg):
             title = lesson.get("lesson_title", "No Lesson Title")
             for c in lesson.get("lesson_contents", []):
                 if c.get("content_status") != "Queue" and c.get("content_url"):
-                    lectures.append(f"{title}] | {item.get('content_title','No Content')}: https://vz-c8c7763d-df6.b-cdn.net/{c['content_url']}/playlist.m3u8")
+                    lectures.append(f"{title} | {item.get('content_title','No Content')}: https://vz-c8c7763d-df6.b-cdn.net/{c['content_url']}/playlist.m3u8")
                     v_count += 1
                     
             for rc in lesson.get("lesson_recorded_classes", []):
                 if rc.get("content_status") != "Queue" and rc.get("recording_url"):
-                    lectures.append(f"{title}] | {rc.get('topic','No Topic')}: https://vz-c8c7763d-df6.b-cdn.net/{rc['recording_url']}/playlist.m3u8")
+                    lectures.append(f"{title} | {rc.get('topic','No Topic')}: https://vz-c8c7763d-df6.b-cdn.net/{rc['recording_url']}/playlist.m3u8")
                     v_count += 1
         
 
