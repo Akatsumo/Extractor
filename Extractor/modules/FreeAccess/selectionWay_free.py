bf5b726073dc5391960844ffdcd4b771e1c684c1
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-10-30 14:30:09 +0530

    Update selectionWay_free.py

--- Extractor/modules/FreeAccess/selectionWay_free.py
+++ Extractor/modules/FreeAccess/selectionWay_free.py
@@ -103,7 +103,7 @@ async def selectionWay_access(_, message, user_id=None):
             
         file_name = f"{batch_name.replace('/', '')}_{user_id}.txt"
         with open(file_name, "w", encoding="utf-8") as f:
-            f.write("\n".join(lectures[::-1]))
+            f.write("\n".join(lectures))
             
         elapsed = main_func.get_time(end_time - start_time)
         caption = (
