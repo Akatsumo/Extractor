3a6d7edb4cc8fced2f787f90d449d954d8b81c2e
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-11-02 13:23:42 +0530

    Update videocrypt.py

--- Extractor/modules/videocrypt.py
+++ Extractor/modules/videocrypt.py
@@ -190,8 +190,8 @@ class VideoCryptExtractor:
             data = {"user_id": userId}
             courses_data = self.fetch("data_model/course/get_my_courses", headers, data, key, iv)
             courses = courses_data.get("data", [])
-            if not courses:
-                return await msg.edit_text("No Batch Data found!!")
+            # if not courses:
+            #     return await msg.edit_text("No Batch Data found!!")
 
             course_batches = "📚 **Available Batches:**\n\n"
             for c in courses:
