c1b454d99b73d9778f00459b124c1b6b17608024
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-11-06 15:43:07 +0530

    Update jchemistry_free.py

--- Extractor/modules/FreeAccess/jchemistry_free.py
+++ Extractor/modules/FreeAccess/jchemistry_free.py
@@ -1,10 +1,10 @@
-import os, time
+import os
+import time
 import asyncio
-import requests 
+import requests
 from Extractor import app
 from Extractor.core import main_func
 from pyromod.exceptions import ListenerTimeout
-
 
 
 # --------------------------- Course-Content --------------------------- #
@@ -12,36 +12,55 @@ from pyromod.exceptions import ListenerTimeout
 async def course_content(session, batch_id, course_ids):
     lectures, v_count, p_count = [], 0, 0
     for id in course_ids:
-      response_data = session.get(f"https://jchemistry-api.edmingle.com/nuSource/api/v1/public/tutor/class/curriculum/{id}?institution_bundle_id={batch_id}")
-      if response_data.status_code != 200:
-        continue
-        resources_data = response_data.json().get("course_curriculum", {}).get("resources", [])
+        response_data = session.get(
+            f"https://jchemistry-api.edmingle.com/nuSource/api/v1/public/tutor/class/curriculum/{id}?institution_bundle_id={batch_id}"
+        )
+        if response_data.status_code != 200:
+            continue
+
+        resources_data = (
+            response_data.json().get("course_curriculum", {}).get("resources", [])
+        )
         if not resources_data:
-          continue
-        for resourse in resources_data:
-          section_name = resources.get("section_name", "N/A")
-          for rs in resources.get("resources", []):
-            material_name = rs.get("material_name", "N/A")
-            material_type = rs.get("type", "N/A")
-            is_drm = rs.get("is_drm")
-            drm_url = rs.get("drm_url")
-            vimeo_url = rs.get("vimeo_url")
-            is_enterprise_drm = rs.get("is_enterprise_drm")
-            vdocipher_video_id = rs.get("vdocipher_video_id")
-            is_videocrypt_drm = rs.get("is_videocrypt_drm")
-            videocrypt_video_id = rs.get("videocrypt_video_id")
-            if material_type == "video/mp4":
-              url = vimeo_url if vimeo_url else drm_url if is_drm else vdocipher_video_id if is_enterprise_drm  else  videocrypt_video_id if is_videocrypt_drm else None
-              if url:
-                v_count += 1
-                lecture.append(f"{section_name} | {material_name}: {url}")
-              
-            if material_type == "application/pdf":
-              pdf_url = vimeo_url if vimeo_url else drm_url if is_drm else vdocipher_video_id if is_enterprise_drm  else  videocrypt_video_id if is_videocrypt_drm else None
-              if pdf_url:
-                p_count += 1
-                lecture.append(f"{section_name} | {material_name}: {url}")
-        
+            continue
+
+        for resource in resources_data:
+            section_name = resource.get("section_name", "N/A")
+            for rs in resource.get("resources", []):
+                material_name = rs.get("material_name", "N/A")
+                material_type = rs.get("type", "N/A")
+                is_drm = rs.get("is_drm")
+                drm_url = rs.get("drm_url")
+                vimeo_url = rs.get("vimeo_url")
+                is_enterprise_drm = rs.get("is_enterprise_drm")
+                vdocipher_video_id = rs.get("vdocipher_video_id")
+                is_videocrypt_drm = rs.get("is_videocrypt_drm")
+                videocrypt_video_id = rs.get("videocrypt_video_id")
+
+                if material_type == "video/mp4":
+                    url = (
+                        vimeo_url
+                        or drm_url if is_drm
+                        else vdocipher_video_id if is_enterprise_drm
+                        else videocrypt_video_id if is_videocrypt_drm
+                        else None
+                    )
+                    if url:
+                        v_count += 1
+                        lectures.append(f"{section_name} | {material_name}: {url}")
+
+                elif material_type == "application/pdf":
+                    pdf_url = (
+                        vimeo_url
+                        or drm_url if is_drm
+                        else vdocipher_video_id if is_enterprise_drm
+                        else videocrypt_video_id if is_videocrypt_drm
+                        else None
+                    )
+                    if pdf_url:
+                        p_count += 1
+                        lectures.append(f"{section_name} | {material_name}: {pdf_url}")
+
     return lectures, v_count, p_count
 
 
@@ -50,30 +69,34 @@ async def course_content(session, batch_id, course_ids):
 async def jchemistry_access(_, message, user_id=None):
     user_id = user_id if user_id else message.from_user.id
     session = requests.Session()
+    msg = await message.reply_text("**Fetching All J Chemistry Batches, Please Wait...**")
 
     try:
-        await msg.edit_text("**Fetching All J Chemistry Batches, Please Wait...**")
         response = session.get("https://jchemistry-api.edmingle.com/nuSource/api/v1/institute/157/courses")
         if response.status_code != 200:
-            return await msg.edit_text("Failed to fetch J Chemsitry batches")
+            return await msg.edit_text("Failed to fetch J Chemistry batches")
 
-        course_data = response.json().get('institute_courses', [])
+        course_data = response.json().get("institute_courses", [])
         if not course_data:
             return await msg.edit_text("No course data found.")
 
         batch_data = []
         batch_list = "📚 **Available Batches:**\n\n"
         for course in course_data:
-            course_bundle = course.get('course_bundles')
+            course_bundle = course.get("course_bundles")
             if not course_bundle:
-              continue
-            batch_data.append({"course_id": course_bundle.get('institution_bundle_id'), "name": course_bundle.get('bundle_name'), "course_ids": course_bundle.get('course_ids')})
+                continue
+            batch_data.append({
+                "course_id": course_bundle.get("institution_bundle_id"),
+                "name": course_bundle.get("bundle_name"),
+                "course_ids": course_bundle.get("course_ids"),
+            })
             batch_list += f"`{course_bundle.get('institution_bundle_id')}` - **{course_bundle.get('bundle_name')}**\n"
-            
+
         thumb = await main_func.send_file(app, file_name=None, user_id=None, caption=None, thumb=None, onlyThumb=True)
         caption = "**📊 Now send the Batch ID to Download**"
         batch_file = None
-        
+
         if len(batch_list) > 4000:
             batch_list_name = f"Jchemistry_batchList_{user_id}.txt"
             with open(batch_list_name, "w", encoding="utf-8") as f:
@@ -89,7 +112,8 @@ async def jchemistry_access(_, message, user_id=None):
         if batch_file:
             await batch_file.delete()
 
-        batch_name, course_ids = next(((course["name"], course["course_ids"]) for course in batch_data if str(course["course_id"]) == batch_id), (None, None))
+        batch_name, course_ids = next(((course["name"], course["course_ids"]) for course in batch_data if str(course["course_id"]) == batch_id),(None, None),)
+
         if not batch_name:
             return await msg.edit_text("**Invalid Batch ID. Please try again.**")
         if not course_ids:
