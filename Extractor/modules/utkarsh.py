c8717096aba347c3fb944344adc4a4acacc05171
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-11-09 09:33:06 +0530

    Update utkarsh.py

--- Extractor/modules/utkarsh.py
+++ Extractor/modules/utkarsh.py
@@ -224,7 +224,7 @@ class UtkarshExtractor:
             end_time = time.time()
             file_name = f"{batch_name.replace('/', '')}_{user_id}.txt"
             with open(file_name, "w", encoding="utf-8") as f:
-                f.write(lectures)
+                f.write("\n".join(lectures))
                 
             elapsed = main_func.get_time(end_time - start_time)
             caption = (
