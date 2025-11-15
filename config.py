021fa46c2478a6cb7def23b884beff3c51f8a209
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-11-15 23:01:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from os import getenv
 API_ID = int(getenv("API_ID", "26850449"))
 API_HASH = getenv("API_HASH", "72a730c380e68095a8549ad7341b0608")
 BOT_TOKEN = getenv("BOT_TOKEN", "8220513775:AAEV3R4IcJ5iIX3DfjIjtAAchUFoQlJOFw8")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "8462359928 6414073613 6462391456").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "8462359928 7980470023 6414073613 6462391456").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://Extractor:ohUUMrBcLaCxlWZv@cluster0.nsjqx8b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 CHANNEL_ID = int(getenv("CHANNEL_ID", "-1003190826843"))
