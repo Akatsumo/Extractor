7c22f8cfab14bb3ffa3719d3875a04384f6aafcb
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2025-12-22 20:40:19 +0530

    Update cdsjourney_free.py

--- Extractor/modules/FreeAccess/cdsjourney_free.py
+++ Extractor/modules/FreeAccess/cdsjourney_free.py
@@ -69,9 +69,8 @@ async def cdsjourney_access(_, message, user_id=None):
             "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
             "Accept-Encoding": "gzip"
         }
-        # token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzY2MzQwMjc2LCJpYXQiOjE3NjExNTYyNzYsImp0aSI6ImZiMjFlYjA4MmJiNjRhYTk5MmQ0N2M0NjE5YjNkYmQ3IiwidXNlcl9pZCI6NDE5NDEyfQ.rmpi91wHnhn3NiHmfyQEJ0CeqB2dZ6qGUgEMMDlqQxI"
-
-        token = None
+        token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzY2MzQwMjc2LCJpYXQiOjE3NjExNTYyNzYsImp0aSI6ImZiMjFlYjA4MmJiNjRhYTk5MmQ0N2M0NjE5YjNkYmQ3IiwidXNlcl9pZCI6NDE5NDEyfQ.rmpi91wHnhn3NiHmfyQEJ0CeqB2dZ6qGUgEMMDlqQxI"
+        
         if not token:
             await msg.edit_text("Enter your login Gmail:")
             input1 = await app.listen(user_id=user_id, timeout=30)
