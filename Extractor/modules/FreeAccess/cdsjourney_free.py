17f2c4661d57a4ea9c82277753ef84a4e8768aa4
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Akatsumo@users.noreply.github.com>
2026-02-24 13:37:00 +0530

    Update cdsjourney_free.py

--- Extractor/modules/FreeAccess/cdsjourney_free.py
+++ Extractor/modules/FreeAccess/cdsjourney_free.py
@@ -67,12 +67,18 @@ async def cdsjourney_access(_, message, user_id=None):
     session = requests.Session()
     try:
         msg = await message.reply_text("**Fetching CDS Journey All Batches, Please Wait...**")
+        # headers = {
+        #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
+        #     "Accept": "application/json",
+        #     "Content-Type": "application/json",
+        #     "Origin": "https://www.cdsjourney.com",
+        #     "Referer": "https://www.cdsjourney.com/"
+        # }
         headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-            "Accept": "application/json",
-            "Content-Type": "application/json",
-            "Origin": "https://www.cdsjourney.com",
-            "Referer": "https://www.cdsjourney.com/"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                          "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+            "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
+            "Accept-Encoding": "gzip"
         }
         token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzc3MTAzNjY4LCJpYXQiOjE3NzE5MTk2NjgsImp0aSI6ImEwZDYwNWE1NjAwYjQ4NWRiZTkwZDQ3YmM4NGQzM2FiIiwidXNlcl9pZCI6NDU2Nzk3fQ.E8WWQIhrDLnuWS1Z0BlYyu75BDFRTqklrQkeSlDAz5I"
         
