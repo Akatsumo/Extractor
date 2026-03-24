c5f62ba419107b810ca1a79f5d13e5bf9674663b
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Akatsumo@users.noreply.github.com>
2026-03-24 18:41:34 +0530

    Update core_func.py

--- Extractor/core/core_func.py
+++ Extractor/core/core_func.py
@@ -6,46 +6,46 @@ chandraInstitute_free, jchemistry_free, pathfinder_free)
 
 # ---------------------------------- AppName-Dict ---------------------------------- #
 appNameDict = {
-    "khan_": {"name": "Khan", "func": khan.khan_handler},
-    "careerwill_": {"name": "Careerwill", "func": careerwill.careerwill_login},
-    "adda247_": {"name": "Adda 247", "func": adda247.adda_login},
-    "classplus_": {"name": "Classplus", "func": classplus.classplus_login},
-    "vajiram_": {"name": "Vajiram", "func": vajiramias.vajiram_login},
-    "videocrypt_": {"name": "Videocrypt", "func": videocrypt.videocrypt_login},
+    "khan_": {"name": "ᴋʜᴀɴ", "func": khan.khan_handler},
+    "careerwill_": {"name": "ᴄᴀʀᴇᴇʀᴡɪʟʟ", "func": careerwill.careerwill_login},
+    "adda247_": {"name": "ᴀᴅᴅᴀ 247", "func": adda247.adda_login},
+    "classplus_": {"name": "ᴄʟᴀꜱꜱᴘʟᴜꜱ", "func": classplus.classplus_login},
+    "vajiram_": {"name": "ᴠᴀᴊɪʀᴀᴍ", "func": vajiramias.vajiram_login},
+    "videocrypt_": {"name": "ᴠɪᴅᴇᴏᴄʀʏᴘᴛ", "func": videocrypt.videocrypt_login},
 }
 
 # ---------------------------------- WithoutApp-Dict ---------------------------------- #
 WithoutAppDict = {
-    "wcdsjourney_": {"name": "Cds Journey", "func": cdsjourney_free.cdsjourney_access},
-    "wclassplus_": {"name": "Classplus", "func": classplus_free.classplus_access},
-    "wcivilguruji_": {"name": "Civil Guruji", "func": civilguruji_free.civilguruji_access},
-    "wstudyiq_": {"name": "Study IQ", "func": studyiq_free.studyiq_access},
-    "wqualityEducation_": {"name": "Quality Education", "func": qualityEducation_free.qualityEducation_access},
-    "wtestpaper_": {"name": "Test Paper", "func": testpaper_free.testpaper_access},
-    "wjincjodhpur_": {"name": "Jinc Jodhpur", "func": jincjodhpur_free.jincJodhpur_access},
-    "wgeologicalConcepts_": {"name": "Geological Concepts", "func": geologicalConcepts_free.geologicalConcepts_access},
-    "wselectionway_": {"name": "Selection Way", "func": selectionWay_free.selectionWay_access},
-    "wtaiyarikrlo_": {"name": "Taiyari Krlo", "func": taiyariKarlo_free.taiyarKarlo_access},
-    "wsscpinnacle_": {"name": "SSC Pinnacle", "func": sscpinnacle_free.sscpinnacle_access},
-    "wchandraInstitute_": {"name": "Chandra Institute", "func": chandraInstitute_free.chandraInstitute_access},
-    "wjchemistry_": {"name": "J Chemistry", "func": jchemistry_free.jchemistry_access},
-    "wpathfinder_": {"name": "Path Finder", "func": pathfinder_free.patherfinder_access},
-    "wutkarsh_": {"name": "Utkarsh", "func": utkarsh.utkarsh_start},
-    # "wjrfadda_": {"name": "Jrf Adda", "func": jrfadda_free.jrfadda_access},
+    "wcdsjourney_": {"name": "ᴄᴅꜱ ᴊᴏᴜʀɴᴇʏ", "func": cdsjourney_free.cdsjourney_access},
+    "wclassplus_": {"name": "ᴄʟᴀꜱꜱᴘʟᴜꜱ", "func": classplus_free.classplus_access},
+    "wcivilguruji_": {"name": "ᴄɪᴠɪʟ ɢᴜʀᴜᴊɪ", "func": civilguruji_free.civilguruji_access},
+    "wstudyiq_": {"name": "ꜱᴛᴜᴅʏ ɪQ", "func": studyiq_free.studyiq_access},
+    "wqualityEducation_": {"name": "Qᴜᴀʟɪᴛʏ ᴇᴅᴜᴄᴀᴛɪᴏɴ", "func": qualityEducation_free.qualityEducation_access},
+    "wtestpaper_": {"name": "ᴛᴇꜱᴛ ᴘᴀᴘᴇʀ", "func": testpaper_free.testpaper_access},
+    "wjincjodhpur_": {"name": "ᴊɪɴᴄ ᴊᴏᴅʜᴘᴜʀ", "func": jincjodhpur_free.jincJodhpur_access},
+    "wgeologicalConcepts_": {"name": "ɢᴇᴏʟᴏɢɪᴄᴀʟ ᴄᴏɴᴄᴇᴘᴛꜱ", "func": geologicalConcepts_free.geologicalConcepts_access},
+    "wselectionway_": {"name": "ꜱᴇʟᴇᴄᴛɪᴏɴ ᴡᴀʏ", "func": selectionWay_free.selectionWay_access},
+    "wtaiyarikrlo_": {"name": "ᴛᴀɪʏᴀʀɪ ᴋʀʟᴏ", "func": taiyariKarlo_free.taiyarKarlo_access},
+    "wsscpinnacle_": {"name": "ꜱꜱᴄ ᴘɪɴɴᴀᴄʟᴇ", "func": sscpinnacle_free.sscpinnacle_access},
+    "wchandraInstitute_": {"name": "ᴄʜᴀɴᴅʀᴀ ɪɴꜱᴛɪᴛᴜᴛᴇ", "func": chandraInstitute_free.chandraInstitute_access},
+    "wjchemistry_": {"name": "ᴊ ᴄʜᴇᴍɪꜱᴛʀʏ", "func": jchemistry_free.jchemistry_access},
+    "wpathfinder_": {"name": "ᴘᴀᴛʜ ꜰɪɴᴅᴇʀ", "func": pathfinder_free.patherfinder_access},
+    "wutkarsh_": {"name": "ᴜᴛᴋᴀʀꜱʜ", "func": utkarsh.utkarsh_start},
+    # "wjrfadda_": {"name": "ᴊʀꜰ ᴀᴅᴅᴀ", "func": jrfadda_free.jrfadda_access},
 }
 
 
 # ---------------------------------- VideocryptApp-Dict ---------------------------------- #
 videoCryptDict = {
-    "abhinaymaths_": {"name": "Abhinay Maths", "func": videocrypt},
-    "eduteria_": {"name": "Eduteria", "func": videocrypt},
-    "kotamentors_": {"name": "Kota Mentors", "func": videocrypt},
-    "missionselection_": {"name": "Mission Selection", "func": videocrypt},
-    "rankbuddy_": {"name": "Rank Buddy", "func": videocrypt},
-    "rajputtutorials_": {"name": "Rajput Tutorials", "func": videocrypt},
-    "pateltutorials_": {"name": "Patel Tutorials", "func": videocrypt},
-    "mypathsala_": {"name": "My Pathsala", "func": videocrypt},
-    "nexttoppers_": {"name": "Next Toppers", "func": videocrypt},
+    "abhinaymaths_": {"name": "ᴀʙʜɪɴᴀʏ ᴍᴀᴛʜꜱ", "func": videocrypt},
+    "eduteria_": {"name": "ᴇᴅᴜᴛᴇʀɪᴀ", "func": videocrypt},
+    "kotamentors_": {"name": "ᴋᴏᴛᴀ ᴍᴇɴᴛᴏʀꜱ", "func": videocrypt},
+    "missionselection_": {"name": "ᴍɪꜱꜱɪᴏɴ ꜱᴇʟᴇᴄᴛɪᴏɴ", "func": videocrypt},
+    "rankbuddy_": {"name": "ʀᴀɴᴋ ʙᴜᴅᴅʏ", "func": videocrypt},
+    "rajputtutorials_": {"name": "ʀᴀᴊᴘᴜᴛ ᴛᴜᴛᴏʀɪᴀʟꜱ", "func": videocrypt},
+    "pateltutorials_": {"name": "ᴘᴀᴛᴇʟ ᴛᴜᴛᴏʀɪᴀʟꜱ", "func": videocrypt},
+    "mypathsala_": {"name": "ᴍʏ ᴘᴀᴛʜꜱᴀʟᴀ", "func": videocrypt},
+    "nexttoppers_": {"name": "ɴᴇxᴛ ᴛᴏᴘᴘᴇʀꜱ", "func": videocrypt},
 }
 
 
